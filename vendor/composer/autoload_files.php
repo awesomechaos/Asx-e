bc3ad2140fab4d0aa9f90b9c5074c85f35443a46
Ian Zhang <awesomechaos@qq.com>
2016-07-15 22:36:27 +0800

    up

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,13 +6,16 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     $vendorDir . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
     $vendorDir . '/symfony/var-dumper/Resources/functions/dump.php',
-    $vendorDir . '/nikic/php-parser/lib/bootstrap.php',
-    $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
-    $vendorDir . '/psy/psysh/src/Psy/functions.php',
     $vendorDir . '/danielstjules/stringy/src/Create.php',
+    $vendorDir . '/illuminate/support/helpers.php',
+    $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
+    $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
+    $vendorDir . '/psy/psysh/src/Psy/functions.php',
+    $vendorDir . '/paragonie/random_compat/lib/random.php',
+    $vendorDir . '/illuminate/html/helpers.php',
     $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
     $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
-    $vendorDir . '/illuminate/html/helpers.php',
 );
